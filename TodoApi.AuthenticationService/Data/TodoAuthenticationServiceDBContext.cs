28217a77b2d9d0de393173f8e655c755cf2bb067
barnabas <barnabas.mwendwa@centrino.co.ke>
2024-03-25 12:10:21 +0300

    Add a DB Context class for the TodoApi Authentication Service

---
+++ TodoApi.AuthenticationService/Data/TodoAuthenticationServiceDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.AuthenticationService.Models;
+
+namespace TodoApi.AuthenticationService.Data
+{
+    public class TodoAuthenticationServiceDBContext : DbContext
+    {
+        public TodoAuthenticationServiceDBContext(DbContextOptions<TodoAuthenticationServiceDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
